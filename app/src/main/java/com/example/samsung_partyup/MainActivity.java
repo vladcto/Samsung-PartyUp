8712b31d305f59e8dbe5dfa2706da0032aa51927
vladcto <razrab.ytka@gmail.com>
2020-02-22 18:30:45 +0300

    Создал проект и разметку list_party.xml для RecyclerView.

---
+++ app/src/main/java/com/example/samsung_partyup/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.samsung_partyup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
