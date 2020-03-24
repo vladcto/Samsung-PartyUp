d4a81cf87b6b18342bde50618cc03d9f9e3c487a
vladcto <razrab.ytka@gmail.com>
2020-03-24 23:54:15 +0300

    Обновил все изобр. в соответствие с фигмой. Починил кнопку "+", ранее не отображалась поверх party.

--- app/src/main/java/com/example/samsung_partyup/MainActivity.java
+++ app/src/main/java/com/example/samsung_partyup/MainActivity.java
@@ -24,18 +24,18 @@ public class MainActivity extends AppCompatActivity {
         adapter.setItems(new PartyInfo(R.drawable.test,"Смотрим Тарантино на Невском",
                 "Россия,Санкт-Петербург","04 апр.",
                 "21:45","127 м.","a"));
-        adapter.setItems(new PartyInfo(R.drawable.test,"aваыwrerwerewrwerwerwerwerwerwerwerwerewrwrewrrwerewrwre",
-                "12","b",
-                "f","s","a"));
-        adapter.setItems(new PartyInfo(R.drawable.test,"aыва",
-                "12","b",
-                "f","s","a"));
-        adapter.setItems(new PartyInfo(R.drawable.test,"aфыввфы",
-                "12","b",
-                "f","s","a"));
+        adapter.setItems(new PartyInfo(R.drawable.ramstain,"Раммштайн",
+                "Россия,Санкт-Петербург,Футбольная ал., дом 8","12 апр.",
+                "19:00","11,5 км.","N/A"));
+        adapter.setItems(new PartyInfo(R.drawable.lera,"Вписка у Леры",
+                "Россия, Санкт-Петербург,ул. Рубинштейна, дом 4321","04 апр.",
+                "23:30","351 м.","N/A"));
+        adapter.setItems(new PartyInfo(R.drawable.birthday,"День Рождения Лауры",
+                "Россия, Санкт-Петербург, 2-я Советская ул., 27/2 A","05 апр.",
+                "22:00","874 м.","N/A"));
         adapter.setItems(new PartyInfo(R.drawable.test,"уй123фвыa",
                 "12","b",
-                "f","s","a"));
+                "f","s","N/A"));
     }
 
     public void startPartyInfo(Intent intent){
