f6d04ec56d19c0a5861efdb9926be0f6b6a1dd12
vladcto <razrab.ytka@gmail.com>
2020-03-28 20:47:54 +0300

    Обновил дизайн в соответствие с фигмой.

--- app/src/main/java/com/example/samsung_partyup/MainActivity.java
+++ app/src/main/java/com/example/samsung_partyup/MainActivity.java
@@ -23,19 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
         adapter.setItems(new PartyInfo(R.drawable.test,"Смотрим Тарантино на Невском",
                 "Россия,Санкт-Петербург","04 апр.",
-                "21:45","127 м.","a",new String[]{"19+","Бесплатно","dasd","sds"}));
+                "21:45","127 м.","a",new String[]{"18+","Бесплатно","Вечеринка"}));
         adapter.setItems(new PartyInfo(R.drawable.ramstain,"Раммштайн",
                 "Россия,Санкт-Петербург,Футбольная ал., дом 8","12 апр.",
-                "19:00","11,5 км.","N/A"));
+                "19:00","11,5 км.","N/A",new String[]{"18+","Концерт","Платно","Рок"}));
         adapter.setItems(new PartyInfo(R.drawable.lera,"Вписка у Леры",
                 "Россия, Санкт-Петербург,ул. Рубинштейна, дом 4321","04 апр.",
-                "23:30","351 м.","N/A"));
+                "23:30","351 м.","N/A",new String[]{"18+","Бесплатно","Вечеринка"}));
         adapter.setItems(new PartyInfo(R.drawable.birthday,"День Рождения Лауры",
                 "Россия, Санкт-Петербург, 2-я Советская ул., 27/2 A","05 апр.",
-                "22:00","874 м.","N/A"));
-        adapter.setItems(new PartyInfo(R.drawable.test,"уй123фвыa",
-                "12","b",
-                "f","s","N/A"));
+                "22:00","874 м.","N/A",new String[]{"21+","Бесплатно","Вечеринка"}));
     }
 
     public void startPartyInfo(Intent intent){
